82e9708ed65bb4370b71cbfe43dc054058167ae8
Timzonatimothy@gmail.com <Timzonatymothy@gmail.com>
2021-03-01 11:42:37 +0100

    portfolio

---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
+from django.core.mail import send_mail
+# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['about'] = About.objects.first()
+        context['services'] = Service.objects.all()
+        context['works'] = RecentWork.objects.all()
+        context['client'] = Client.objects.all()
+        return context
+
+def contact(request):
+    if request.method == "POST":
+        massage_name = request.POST['massage_name'] 
+        massage_email = request.POST['email']
+        massage = request.POST['massage']
+        #send mai
+        send_mail = (
+            massage_name,
+            massage_email,
+            massage,['arizonatymothy@gmail.com','timzonatimothy@gamil.com']
+        )
+        return render(request, 'contact.html', {'massage_name':massage_name,'massage_email':massage_email,'massage':massage})
+
+    else:
+        return render(request, 'contact.html', {})
